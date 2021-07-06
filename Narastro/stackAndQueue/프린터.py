f16f32357cc3bdc7a0e985df12720eb9daf06dea
Narastro <advice02@naver.com>
2021-07-06 22:46:16 +0900

    Deque - Programmers High score kit - Stack/Queue

---
+++ Narastro/stackAndQueue/프린터.py
@@ -0,0 +1,31 @@
+# 2021.07.06. 2021 Programmers High score kit - Stack and Queue
+# 프린터
+
+import collections
+def solution(priorities, location):
+    answer = 1
+    prior_deq = collections.deque(priorities)
+    max_prior = max(prior_deq)
+    find = False
+
+    while not find:
+        for i in  range(location+1):
+            v = prior_deq.popleft()
+            # 1. 인쇄
+            if max_prior == v:
+                # 정답을 찾은 경우
+                if i == location:
+                    find = True
+                    break
+                answer += 1
+                max_prior = max(prior_deq)
+            # 2. 대기
+            else:
+                prior_deq.append(v)
+        # 위치 초기화
+        location = len(prior_deq)-1
+    return answer
+
+print(solution([1,2,3,2,5],3))
+
+
